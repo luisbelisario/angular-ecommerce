3de31fc01949d8a046833ee7f08ff87b0763d178
luisbelisario <luisbls77@hotmail.com>
2023-09-30 17:35:14 -0300

    components modularization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
